2c80773203eb938bb8c10b49298ab86fb0bb9c02
Faviun <lightbringerlal@gmail.com>
2025-03-07 22:31:38 +0200

    32_2

--- 32.js
+++ 32.js
@@ -10,3 +10,18 @@ const function1 = (num) => {
 };
 
 function1(12345);
+
+// Сделайте функцию, которая параметром будет принимать число и удалять из него нули.
+
+const function2 = (num) => {
+    let str = num.toString();
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== "0") {
+            newStr += str[i];
+        }
+    }
+    console.log(+newStr);
+};
+
+function2(102030405060708090);
