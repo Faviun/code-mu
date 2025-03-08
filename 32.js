9a62db70a04fc74deae723f50818f84663a69aa1
Faviun <lightbringerlal@gmail.com>
2025-03-08 21:57:04 +0200

    32_3

--- 32.js
+++ 32.js
@@ -25,3 +25,17 @@ const function2 = (num) => {
 };
 
 function2(102030405060708090);
+
+// Сделайте функцию, которая будет возвращать сколько дней прошло или осталось до заданной даты в году, в зависимости от того, была уже эта дата или нет.
+
+const function3 = (date) => {
+    let now = new Date();
+    let newDate = new Date(date);
+    let diff = newDate - now;
+    let days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    days < 0
+        ? console.log("Прошло " + -days + " дней")
+        : console.log("Осталось " + days + " дней");
+};
+
+function3("2025-03-08");
