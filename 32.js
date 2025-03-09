2666c9c6948085b33ec509015b4029f20344febd
Faviun <lightbringerlal@gmail.com>
2025-03-09 19:30:43 +0200

    32_f

--- 32.js
+++ 32.js
@@ -39,3 +39,58 @@ const function3 = (date) => {
 };
 
 function3("2025-03-08");
+
+// Сделайте функцию, которая параметром будет принимать год и проверять, високосный он или нет.
+
+const function4 = (year) => {
+    year % 4 === 0 ? console.log("Високосный") : console.log("Не високосный");
+};
+
+function4(2025);
+
+// Сделайте функцию, которая вернет массив всех високосных годов за предыдущие сто лет.
+
+const function5 = (year) => {
+    let arr = [];
+    for (let i = year - 100; i <= year; i++) {
+        if (i % 4 === 0) {
+            arr.push(i);
+        }
+    }
+    console.log(arr);
+};
+
+function5(2025);
+
+// Сделайте функцию, которая будет возвращать сколько дней осталось до конца текущего месяца.
+
+const function6 = () => {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth();
+    let lastDay = new Date(year, month + 1, 0).getDate();
+    let days = lastDay - now.getDate();
+    console.log(days);
+};
+
+function6();
+
+// Сделайте функцию, которая вернет предыдущий, текущий и следующий дни недели словом в виде следующего объекта:
+// {
+// 	next: 'пн',
+// 	curr: 'вс',
+// 	prev: 'сб',
+// }
+
+const function7 = () => {
+    let now = new Date();
+    let days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+    let obj = {
+        prev: days[now.getDay() - 1] || days[6],
+        curr: days[now.getDay()],
+        next: days[now.getDay() + 1] || days[0],
+    };
+    console.log(obj);
+};
+
+function7();
