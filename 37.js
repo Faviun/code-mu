37c910c6118cf09903752b7067d94068b5271bc1
Faviun <lightbringerlal@gmail.com>
2025-03-14 20:35:18 +0200

    32_2

--- 37.js
+++ 37.js
@@ -10,3 +10,19 @@ const function1 = (text) => {
 };
 
 function1("hi world press");
+
+// Сделайте функцию, которая параметром будет принимать два массива и возвращать массив их общих элементов.
+
+const function2 = (arr1, arr2) => {
+    const newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                newArr.push(arr1[i]);
+            }
+        }
+    }
+    console.log(newArr);
+};
+
+function2([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
